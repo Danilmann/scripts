7fadbcef60ab67dd89b088705ffa40ab04eac4fa
Danilmann <90003745+Danilmann@users.noreply.github.com>
2024-09-19 10:05:09 +0300

    Update parse-file.sh

--- parse-file.sh
+++ parse-file.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Скрипт для парсинга файла и получения значений переменных
 
-# Путь к файлу pars-file
 FILE_PATH=$1
 
-# Проверяем, что файл существует
 if [ ! -f "$FILE_PATH" ]; then
   echo "Файл $FILE_PATH не найден!"
   exit 1
 fi
 
-# Чтение данных из файла и экспортирование переменных
+# Чтение данных из файла и экспортирование переменных в окружение
 while IFS= read -r line; do
   if [[ ! $line =~ ^# && $line =~ .*=.* ]]; then
-    export "$line"
+    # Разделяем строку на ключ и значение
+    IFS='=' read -r key value <<< "$line"
+    # Экспортируем переменные в окружение
+    echo "export $key='$value'" >> $GITHUB_ENV
   fi
 done < "$FILE_PATH"
 
-# Проверка, что переменные успешно установлены
 echo "Репозиторий: $repository_name"
 echo "Среда: $environment"
