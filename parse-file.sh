2f0219254dd676d10320d6cf3dc5e6ed26e8f6ba
Danilmann <90003745+Danilmann@users.noreply.github.com>
2024-09-19 09:21:49 +0300

    Create parse-file.sh

---
+++ parse-file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Скрипт для парсинга файла и получения значений переменных
+
+# Путь к файлу pars-file
+FILE_PATH=$1
+
+# Проверяем, что файл существует
+if [ ! -f "$FILE_PATH" ]; then
+  echo "Файл $FILE_PATH не найден!"
+  exit 1
+fi
+
+# Чтение данных из файла и экспортирование переменных
+while IFS= read -r line; do
+  if [[ ! $line =~ ^# && $line =~ .*=.* ]]; then
+    export "$line"
+  fi
+done < "$FILE_PATH"
+
+# Проверка, что переменные успешно установлены
+echo "Репозиторий: $repository_name"
+echo "Среда: $environment"
