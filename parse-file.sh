2b1aa0bc58b9ea10bc07ce1532df20f464194922
Danilmann <90003745+Danilmann@users.noreply.github.com>
2024-09-19 17:35:45 +0300

    Update parse-file.sh

--- parse-file.sh
+++ parse-file.sh
@@ -4,6 +4,9 @@
 # Путь к файлу pars-file
 FILE_PATH=$1
 
+# Задаем значение для переменной GITHUB_ENV
+GITHUB_ENV="./GITHUB_ENV"
+
 # Проверяем, что файл существует
 if [ ! -f "$FILE_PATH" ]; then
   echo "Файл $FILE_PATH не найден!"
@@ -14,9 +17,12 @@ fi
 while IFS= read -r line; do
   if [[ ! $line =~ ^# && $line =~ .*=.* ]]; then
     IFS='=' read -r key value <<< "$line"
-    echo "$key='$value'" >> $GITHUB_ENV
+    echo "$key='$value'" >> "$GITHUB_ENV"
   fi
 done < "$FILE_PATH"
+
+# Экспорт переменных в текущую сессию
+export $(cat $GITHUB_ENV | xargs)
 
 # Проверка, что переменные успешно установлены
 echo "Репозиторий: $repository_name"
